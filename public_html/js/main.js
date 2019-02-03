1df34ae1cf71c9743ce39a2629ddfb724c52c99d
Aleksandar Maksimovic <aleksandar.maksimovic2608@gmail.com>
2019-02-03 20:05:55 +0100

    Implement animations on page, smooth scrolling when clicked on nav links and reduce header padding when scroll

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,9 +1,47 @@
-
-  // Look for .hamburger
-  var hamburger = document.querySelector(".hamburger");
+$(document).ready(function () {
   // On click
-  hamburger.addEventListener("click", function() {
+  $(".hamburger").click(function () {
     // Toggle class "is-active"
-    hamburger.classList.toggle("is-active");
-    // Do something else, like open/close menu
+    $(this).toggleClass("is-active");
   });
+
+  //Smooth scroll
+  $(".navbar-nav a").click(function () {
+    $("html, body").animate({
+      scrollTop: $($.attr(this, "href")).offset().top
+    }, 2000);
+    $(".hamburger").removeClass("is-active");
+    $("#main-nav").removeClass("show");
+  });
+
+  //Reduce header padding on scroll
+  $(window).scroll(function () {
+    const scroll = $(window).scrollTop();
+    if (scroll > 50) {
+      $(".main-header").removeClass("py-lg-3 py-md-2").css("background", "rgba(255, 255, 255, 1)");
+    } else {
+      $(".main-header").addClass("py-lg-3 py-md-2").css("background", "rgba(255, 255, 255, 0.1)");
+    }
+  });
+
+  // Animations
+
+  function animation() {
+    const windowHeight = $(window).height();
+    const scrollTop = $(window).scrollTop();
+
+    $('.animation').each(function () {
+      const topPosition = $(this).offset().top;
+      if (topPosition < scrollTop + windowHeight - 300) {
+        const animation = $(this).data('animation');
+        $(this).addClass(animation);
+      }
+    });
+  }
+
+  animation();
+
+  $(window).scroll(function () {
+    animation();
+  });
+});
