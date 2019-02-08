3455065d3d7aa97d4495d7d950722549e3ecf69e
Aleksandar Maksimovic <aleksandar.maksimovic2608@gmail.com>
2019-02-08 23:23:01 +0100

    Improve animations, implement data-spy, implement iOS and android compatibility

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
+
+
   // On click
   $(".hamburger").click(function () {
     // Toggle class "is-active"
@@ -13,36 +15,141 @@ $(document).ready(function () {
     }, 2000);
     $(".hamburger").removeClass("is-active");
     $("#main-nav").removeClass("show");
+
+    linkActive();
   });
+
+
 
   //Reduce header padding on scroll
   $(window).scroll(function () {
+    animation(); // Start animation on window scroll
+    animateHeader(); //Animate header on window scroll
+    linkActive();
+  });
+
+  animation(); // Start animation on window load
+  animateHeader(); //Animate header on window load
+
+  //Header animation function
+  function animateHeader() {
     const scroll = $(window).scrollTop();
     if (scroll > 50) {
       $(".main-header").removeClass("py-lg-3 py-md-2").css("background", "rgba(255, 255, 255, 1)");
     } else {
       $(".main-header").addClass("py-lg-3 py-md-2").css("background", "rgba(255, 255, 255, 0.1)");
     }
-  });
+  }
 
-  // Animations
+  // Animation function
 
   function animation() {
+
     const windowHeight = $(window).height();
-    const scrollTop = $(window).scrollTop();
+    const scrollDown = $(window).scrollTop();
 
     $('.animation').each(function () {
-      const topPosition = $(this).offset().top;
-      if (topPosition < scrollTop + windowHeight - 150) {
-        const animation = $(this).data('animation');
+      const position = $(this).offset().top;
+
+      if (position < scrollDown + windowHeight - 50) {
+        const animation = $(this).attr('data-animation');
+        const delay = $(this).attr('data-delay');
+        $(this).css('animation-delay', delay);
         $(this).addClass(animation);
       }
     });
   }
 
-  animation();
+  // Add overline and removes it
+  function linkActive() {
+    const navLinks = Array.from(document.querySelectorAll(".nav-link"));
+    navLinks.map(navLink => {
+      if (!navLink.classList.contains("active")) {
+        navLink.classList.add("hvr-overline-from-left");
+      } else {
+        navLink.classList.remove("hvr-overline-from-left");
+      }
+    });
+  }
 
-  $(window).scroll(function () {
-    animation();
+  //Forms validation
+  $(function () {
+    $(".contact-form").validate({
+      highlight: function (element) {
+        $(element).closest(".form-group").addClass("has-danger");
+        $(element).addClass("form-control-danger");
+      },
+      unhighlight: function (element) {
+        $(element).closest(".form-group").removeClass("has-danger").addClass("has-success");
+        $(element).removeClass("form-control-danger").addClass("form-control-success");
+      },
+      rules: {
+        name: {
+          required: true,
+          rangelength: [2, 20]
+        },
+        email: {
+          required: true,
+          email: true
+        },
+        phone: {
+          required: true,
+          digits: true
+        },
+        message: {
+          required: true,
+          maxlength: 255
+        }
+      },
+      messages: {
+        name: {
+          required: "This field is required.",
+          rangelength: "This field must contains between 2 adn 20 characters."
+        },
+        email: {
+          required: "This field is required.",
+          email: "Please enter a valid email."
+        },
+        phone: {
+          required: "This field is required.",
+          digits: "Please enter a valid phone number."
+        },
+        message: {
+          required: "This field is required.",
+          maxlength: "This field can't have more than 255 characters."
+        }
+      },
+      errorElement: "p",
+      errorPlacement: function (error, element) {
+        error.appendTo($(element).closest(".form-group").find(".error-msg"));
+      }
+
+    });
+    $(".subscribe-form").validate({
+      highlight: function (element) {
+        $(element).closest(".form-group").addClass("has-danger");
+        $(element).addClass("form-control-danger");
+      },
+      unhighlight: function (element) {
+        $(element).closest(".form-group").removeClass("has-danger").addClass("has-success");
+        $(element).removeClass("form-control-danger").addClass("form-control-success");
+      },
+      rules: {
+        email: {
+          required: true,
+          email: true
+        }
+      },
+      messages: {
+        email: {
+          required: "This field is required.",
+          email: "Please enter a valid email."
+        }
+      },
+      errorElement: "p",
+      errorPlacement: function (error, element) {
+        error.appendTo($(element).closest(".form-group").find(".error-msg"));
+      }
+    });
   });
 });
